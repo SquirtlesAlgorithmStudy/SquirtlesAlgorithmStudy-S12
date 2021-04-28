e8b417b5cc5dce0c207c9ceef8a18a31f624f61b
shwon96 <wsh4872@gmail.com>
2021-04-28 15:39:49 +0900

    210408 / silver 1 / 결혼식

---
+++ 소현/Dynamic Programming/N2482.py
@@ -0,0 +1,17 @@
+# 작성자 : SH_WON_96
+# 2021-04-27
+# 알고리즘 - DP
+# 문제번호 : 2482 색상환
+
+# 초기 입력값 받기
+import sys
+fast_in = sys.stdin.readline
+
+# 원순열?
+
+N = int(fast_in().strip())
+K = int(fast_in().strip())
+result = 0
+
+
+result = result%1000000003
