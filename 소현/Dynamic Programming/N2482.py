6722ead6fa145ab8838d93c441fb605264f8596a
shwon96 <wsh4872@gmail.com>
2021-05-01 21:45:06 +0900

    210501 / gold 4 / 색상환

--- 소현/Dynamic Programming/N2482.py
+++ 소현/Dynamic Programming/N2482.py
@@ -5,6 +5,7 @@
 
 # 초기 입력값 받기
 import sys
+sys.setrecursionlimit(10**9)
 fast_in = sys.stdin.readline
 
 # 원순열?
@@ -13,5 +14,21 @@ N = int(fast_in().strip())
 K = int(fast_in().strip())
 result = 0
 
+dp = [[0]*(N+1) for _ in range(N+1)] #  N숫자ㅇ서 k를 찾는 건가? 살려줘
 
-result = result%1000000003
+def find(n,k,dp):
+    if n/k == 2:
+        return 2
+    if k == 1:
+        return n
+    if dp[n][k] == 0:
+        tmp = find(n-2,k-1,dp) + find(n-1,k,dp)
+        dp[n][k] = tmp
+        return tmp
+    else:
+        return dp[n][k]
+
+if N/K <2:
+    print(0)
+else:
+    print(find(N,K,dp)%1000000003)
