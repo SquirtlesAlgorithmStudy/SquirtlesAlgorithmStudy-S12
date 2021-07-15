d7c7e05e7ea2c3b3081aade3f15c4af3491d78c1
Yujaeyoon <86181002+Yujaeyoon@users.noreply.github.com>
2021-07-15 10:42:47 +0000

    감자

---
+++ 재윤/거스름돈.py
@@ -0,0 +1,13 @@
+price = int(input())
+
+change = 1000 - price
+
+
+coin1 = change // 500
+coin2 = (change % 500) // 100
+coin3 = (change % 500 % 100) // 50
+coin4 = (change % 500 % 100 % 50) //10
+coin5 = (change % 500 % 100 % 50 % 10) // 5
+coin6 = (change % 500 % 100 % 50 % 10 % 5) // 1
+
+answer = coin1 + coin2 + coin3 + coin4 + coin5 + coin6
