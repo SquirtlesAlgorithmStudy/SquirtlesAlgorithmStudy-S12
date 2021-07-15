b5870d36ec652ebe671d16174e32edbda5055782
Yujaeyoon <86181002+Yujaeyoon@users.noreply.github.com>
2021-07-15 10:25:44 +0000

    감자

--- 재윤/practice1.py
+++
@@ -1,7 +0,0 @@
-N = int(input("1<=N<=100 N :"))
-score = []
-
-for i in range(N):
-  a = int(input())
-  score.append(a)
-
---
+++ 재윤/동준이.py
@@ -0,0 +1,30 @@
+N = int(input())
+score = []
+answer = 0
+
+for i in range(N):
+  a = int(input())
+  score.append(a)
+
+#1
+for i in range(N-1, 0, -1):
+  if i == 0:
+    break
+  if score[i] == score[i -1]:
+    answer += 1
+    score[i -1] -= 1
+  elif score[i] < score[i -1]:
+    answer += score[i -1] - score[i] + 1
+    score[i -1] -= score[i -1] - score[i] + 1
+
+print(answer)
+
+#2 시간초과
+for i in range(N-1 , 0, -1):
+  if i == 0 :
+    break
+  while score[i] > score[i-1] :
+    answer += 1
+    score[i-1] -= 1
+    
+
