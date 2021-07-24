d8322a8e02d729bd4270e2dec97250aac02cfd58
Yujaeyoon <86181002+Yujaeyoon@users.noreply.github.com>
2021-07-24 11:30:09 +0000

    감자

---
+++ 재윤/문자열압축.py
@@ -0,0 +1,20 @@
+def solution(s):
+    answer = 0
+    answer += len(s)
+    S = list(s)
+    # 전체 길이에서 줄어든만큼 빼어나감
+    # 2개연속까진 하나 안하나 똑같음
+    # 연속된수는 문자길이의 절반이 최대
+    '''
+    abab = 2ab    4 > 3        1  
+    ababab = 3ab  6 > 3        3
+
+    abcabc = 3abc  6 > 4       2  
+    abcabcabc = 3abc 9 > 4     5 
+    '''
+    for i in range(len(s)):
+      for j in range(len(s)//2):
+        if S[i:i+j] == S[i+j:2*(i+j)] :
+            answer -= (i+j)
+        
+    return answer
